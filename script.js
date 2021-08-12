c5d5415de0c12c3698bef778e788a8cfdb7248a4
Justin <49010512+justin795003@users.noreply.github.com>
2021-08-12 14:40:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){
+    document.getElementById('menu').style.width = '200px';
+    document.getElementById('content').style.marginRight = '250px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('content').style.marginRight = '0';
+}
